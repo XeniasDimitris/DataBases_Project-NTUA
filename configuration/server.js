840fee7b2ff8a40976c3fdade0ba43a65b5ec407
XeniasDimitris <dimitrisxenias@gmail.com>
2019-05-22 16:32:33 +0300

    fix filldatabase

--- configuration/server.js
+++ configuration/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-//console.log(app);
+
 function openServer(){
     app.listen('3000',()=>{
         console.log('Server started on port 3000');
