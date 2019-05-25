57ee3041e986e8b29ea9c4af3eb7da79a7ca34f7
choosen23 <filippou.michail@gmail.com>
2019-05-25 04:32:00 +0300

    $1

--- push.sh
+++ push.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 
+## ena skriptaki pou kanei eukola push
+## ektelite os eksis:
+## ./push arg1 opou:
+## arg1 einai to minima p tha periexei to commit
+## enjoy
+
 git add *
 git commit -m '$1'
 git push origin master
