f1516d47a16d374c0ce5d7f6477f656ab5c51463
choosen23 <filippou.michail@gmail.com>
2019-05-31 13:33:18 +0300

    'aa'

--- configuration/database.js
+++ configuration/database.js
@@ -48,7 +48,7 @@ function CreateDatabase(){
 
     // table category
     sql = 'CREATE TABLE `Baseis2019`.`category` ( `categoryName` VARCHAR(50) NOT NULL , `supercategoryName` VARCHAR(50) ' +
-        'NOT NULL , PRIMARY KEY (`categoryName`)) ENGINE = InnoDB';
+        ' , PRIMARY KEY (`categoryName`)) ENGINE = InnoDB';
     db.query(sql,(err,results)=>{
         if (err) throw err4;
     });
@@ -82,7 +82,7 @@ function CreateDatabase(){
     });
 
     // table temporary_employee
-    sql = 'CREATE TABLE `Baseis2019`.`temporary_employee` ( `empID` INT NOT NULL , `HiringDate` DATE NOT NULL , PRIMARY KEY (`empID`)) '+
+    sql = 'CREATE TABLE `Baseis2019`.`temporary_employee` ( `empID` INT NOT NULL , `ContactNumb` INT NOT NULL , PRIMARY KEY (`empID`)) '+
         'ENGINE = InnoDB;';
     db.query(sql,(err,results)=>{
         if (err) throw err9;
--- configuration/filldatabase.js
+++ configuration/filldatabase.js
@@ -61,11 +61,130 @@ function fillDatabase(db) {
     " ('978-1-891830-19-8', 'Poison', 1997, 623, 'Keira Watson')," +
     " ('978-1-891830-71-6', 'AEIOU', 1963, 324, 'Yvonne Pfaff')," +
     " ('978-1-891830-75-4', '110 Repc', 1987, 456, 'Amber Collier')," +
-    " ('978-1-891830-90-7', 'Korgi', 2011, 187, 'Ella Gardiner');";
+    " ('978-1-891830-90-7', 'Korgi', 2011, 187, 'Ella Gardiner');"
     db.query(sql,(err,results)=>{
         if (err) throw err;
         console.log('Database Filled');
     });
+    //Copies Imports
+    sql = "INSERT INTO `Baseis2019`.`copies` (`ISBN`, `copyNr`, `shelf`) VALUES" +
+        "('978-1-60309-013-1', 4, 3231)," +
+        "('978-1-60309-025-4', 4, 3123)," +
+        "('978-1-60309-047-6', 6, 3131)," +
+        "('978-1-60309-050-6', 8, 3164)," +
+        "('978-1-60309-062-9', 8, 4332)," +
+        "('978-1-60309-301-9', 11, 4332)," +
+        "('978-1-60309-375-0', 11, 4353)," +
+        "('978-1-60309-386-6', 3, 1314)," +
+        "('978-1-60309-386-6', 9, 3213)," +
+        "('978-1-891830-19-8', 9, 3131)," +
+        "('978-1-891830-71-6', 7, 4321)," +
+        "('978-1-891830-71-6', 8, 5432)," +
+        "('978-1-891830-75-4', 4, 3123)," +
+        "('978-1-891830-90-7', 7, 2313);"
+        db.query(sql,(err,results)=>{
+            if (err) throw err;
+            console.log('Database Filled');
+        });
+    //Employee Imports
+    sql = "INSERT INTO `Baseis2019`.`employee` (`empID`, `EFirst`, `ELast`, `salary`) VALUES" +
+        "(1, 'Lizette ', 'Struthers', 800)," +
+        "(2, 'Dinorah', ' Corbett', 812)," +
+        "(3, 'Lin ', 'Laughlin', 854)," +
+        "(4, 'Donte', 'Guerero', 900)," +
+        "(5, 'Monica', 'Bellucci', 10000)," +
+        "(6, 'Melane', 'Craig', 950)," +
+        "(7, 'Alexis', 'Navas', 850)," +
+        "(8, 'Lisa', 'Rank', 680)," +
+        "(9, 'Bonnie', 'Clark', 750)," +
+        "(10, 'Belva', 'Bartch', 860)," +
+        "(11, 'Jamila', 'Adkinson', 940)," +
+        "(12, 'Hung', 'Chin', 745);"
+        db.query(sql,(err,results)=>{
+            if (err) throw err;
+            console.log('Database Filled');
+        });
+    
+    //Permant Employees Imports
+    sql = "INSERT INTO `Baseis2019`.`permanent_employee` (`empID`, `HiringDate`) VALUES" +
+        "(2, '2019-05-18')," +
+        "(4, '2019-05-25')," +
+        "(7, '2019-05-09')," +
+        "(9, '2019-05-03')," +
+        "(11, '2018-05-03')," +
+        "(10, '2019-05-04');"
+        db.query(sql,(err,results)=>{
+            if (err) throw err;
+            console.log('Database Filled');
+        });
+    //Temporary Employees Imports
+    sql = "INSERT INTO `Baseis2019`.`temporary_employee` (`empID`, `ContactNumb`) VALUES" +
+        "(1, 531432465)," +
+        "(3, 212895367)," +
+        "(5, 960858293)," +
+        "(6, 691045898)," +
+        "(8, 292998954)," +
+        "(12, 604441479);"
+        db.query(sql,(err,results)=>{
+            if (err) throw err;
+            console.log('Database Filled');
+        });
+    //Borrows Imports
+    sql = "INSERT INTO `Baseis2019`.`borrows` (`memberID`, `ISBN`, `copyNr`, `date_of_borrowing`, `date_of_return`) VALUES" +
+        "(1, '978-1-891830-19-8', 9, '2019-03-05', '2019-03-27')," +
+        "(1, '978-1-891830-90-7', 7, '2019-05-02', '2019-05-11')," +
+        "(2, '978-1-891830-75-4', 4, '2019-05-05', '2019-05-14')," +
+        "(3, '978-1-891830-90-7', 7, '2019-02-01', '2019-04-02')," +
+        "(4, '978-1-891830-75-4', 4, '2019-04-06', '2019-05-01')," +
+        "(7, '978-1-60309-047-6', 6, '2019-04-15', '2019-04-30')," +
+        "(10, '978-1-60309-047-6', 6, '2019-05-04', '2019-05-18');"
+        db.query(sql,(err,results)=>{
+            if (err) throw err;
+            console.log('Database Filled');
+        });
+    //Category Imports
+    sql = "INSERT INTO `Baseis2019`.`category` (`categoryName`, `supercategoryName`) VALUES" +
+        "('Art', NULL)," +
+        "('Kids', NULL)," +
+        "('Mystery', NULL)," +
+        "('Science', NULL)," +
+        "('Drawing', 'Art')," +
+        "('Music', 'Art')," +
+        "('Cartoon', 'Kids')," +
+        "('Comics', 'Kids')," +
+        "('Adventure', 'Mystery')," +
+        "('Maths', 'Science')," +
+        "('Physics', 'Science');"
+        db.query(sql,(err,results)=>{
+            if (err) throw err;
+            console.log('Database Filled');
+        });
+    //Belongs to Imports
+    sql = "INSERT INTO `Baseis2019`.`belongs_to` (`ISBN`, `categoryName`) VALUES" +
+        "('978-1-60309-013-1', 'Adventure')," +
+        "('978-1-60309-025-4', 'Art')," +
+        "('978-1-60309-047-6', 'Cartoon')," +
+        "('978-1-60309-050-6', 'Adventure')," +
+        "('978-1-60309-050-6', 'Comics')," +
+        "('978-1-60309-050-6', 'Drawing')," +
+        "('978-1-60309-375-0', 'Cartoon')," +
+        "('978-1-60309-375-0', 'Science')," +
+        "('978-1-60309-386-6', 'Kids')," +
+        "('978-1-891830-19-8', 'Comics')," +
+        "('978-1-891830-71-6', 'Music')," +
+        "('978-1-891830-71-6', 'Science')," +
+        "('978-1-891830-75-4', 'Physics')," +
+        "('978-1-891830-90-7', 'Science');"
+        db.query(sql,(err,results)=>{
+            if (err) throw err;
+            console.log('Database Filled');
+        });
+
+
+
+
+
+
 };
 
 module.exports.fillDatabase = fillDatabase;
