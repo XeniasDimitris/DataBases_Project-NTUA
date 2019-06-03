5a216eae2e37ee89b02f559eebb3d07ee6329d34
XeniasDimitris <dimitrisxenias@gmail.com>
2019-06-04 00:24:25 +0300

    jimbo

--- configuration/createViews.js
+++ configuration/createViews.js
@@ -9,13 +9,12 @@ function createViews(db) {
         console.log('View created');
     });
 
-    /*sql = "CREATE VIEW Baseis2019.Authors_Statistics AS SELECT " +
-    " a.AFirst , a.ALast FROM Baseis2019.author a INNER JOIN Baseis2019.written_by w ON a.authID = w.authID GROUP BY a.authorID;"; 
+    sql = "CREATE VIEW Baseis2019.Authors_Statistics AS SELECT " +
+    " a.AFirst , a.ALast, COUNT(w.ISBN) FROM Baseis2019.author a INNER JOIN Baseis2019.written_by w ON a.authID = w.authID GROUP BY a.authID;"; 
     db.query(sql,(err,results)=>{
         if (err) throw err35;
         console.log('View created');
-    });*/
-    //a.AFirst , a.ALast COUNT(w.ISBN)
+    });
 //STATISTICS
 
 // CREATE VIEW cntbooks AS
