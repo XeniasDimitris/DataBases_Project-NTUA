18e5267efd9d2b05d4ebdea92a1cfb3c6869a215
choosen23 <filippou.michail@gmail.com>
2019-06-03 14:42:12 +0300

    lala

--- configuration/database.js
+++ configuration/database.js
@@ -19,24 +19,14 @@ db.connect((err)=>{
 });
 
 //FUNCTIONS    
-function CreateConnection() {createConnection.createConnection();}
 function CreateDatabase() {createDatabase.createDatabase(db);};
 function CreateForeignKeys() {createforeignKeys.createforeignKeys(db);}
 function CreateTriggers() {createTriggers.createTriggers(db);}
 function FillDatabase() {fillDatabase.fillDatabase(db);}
 
-
-// function CreateViews() {
-//     createViews.createViews(db);
-// }   
-
 //EXPORTS
-module.exports.CreateConnection = CreateConnection();
 module.exports.db = db;
 module.exports.CreateDatabase = CreateDatabase();
 module.exports.CreateForeignKeys = CreateForeignKeys();
 module,exports.CreateTriggers = CreateTriggers();
 module.exports.FillDatabase = FillDatabase();
-
-
-//module.exports.CreateViews = CreateViews();
